69e2abb0402b950a8d8244ff635a65b0d99b057e
Sabyrzhann <92504169+Sabyrzhann@users.noreply.github.com>
2021-10-14 12:01:49 +0600

    Add files via upload

---
+++ MidtermProject/script.js
@@ -0,0 +1,20 @@
+ AOS.init({
+  disable: false, 
+  startEvent: 'DOMContentLoaded', 
+  initClassName: 'aos-init', 
+  animatedClassName: 'aos-animate', 
+  useClassNames: false, 
+  disableMutationObserver: false, 
+  debounceDelay: 50, 
+  throttleDelay: 99, 
+  
+
+  offset: 100, 
+  delay: 200, 
+  duration: 800,
+  easing: 'ease', 
+  once: false, 
+  mirror: false, 
+  anchorPlacement:'top-bootom',
+
+});
